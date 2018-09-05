616e98b4d6aa3b600ce045996e2a7279a05b64da
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-09-05 15:03:04 +0200

    Add external memory module

--- src/main.c
+++ src/main.c
@@ -3,17 +3,22 @@
 #endif
 
 #include <util/delay.h>
+#include <stdio.h>
 #include "uart.h"
+#include "external_memory.h"
 
 #define BAUD (F_CPU/16/9600-1)
 
+
 int main(){
        USART_init(BAUD);
-       while(1 == 1) {
-           USART_putchar('H');
-           USART_putchar('E');
-           USART_putchar('S');
-           USART_putchar('\n');
+       init_external_memory();
+       while(1 == 1) { 
+            printf("HES\n");
+            printf("er\n");
+            printf("spektakulære\n");
+            unsigned char* p = (unsigned char *) 0x1000;
+            *p = 'E';
            _delay_ms(1000);
        }
 
